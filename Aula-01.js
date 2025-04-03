b8911a596578be1052a1fccb21143ff01d3ddfa6
douglaslegramante <douglas.legramante@ifro.edu.br>
2025-04-03 16:26:19 -0400

    Variáveis e tipos de dados

--- Aula-01.js
+++ Aula-01.js
@@ -1,1 +1,7 @@
-//Variáveis e tipos de dados
+let valor = 13.141516;
+let text = "Meu primeiro projeto em JavaScript";
+let ativo = true;
+
+console.log(valor.toFixed(2)); //Arredonda o valor para duas casas decimais
+console.log(text.toUpperCase()); //Converte o texto para maiúsculas
+console.log(ativo.toString()); //Converte para texto
